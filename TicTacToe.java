28a40833067884946fe75dacaec153ce1b809618
moshtab <mdmohsin41439@gmail.com>
2021-08-01 21:44:25 +0530

    uc2 choosing x or y for players

--- TicTacToe.java
+++ TicTacToe.java
@@ -6,8 +6,15 @@ public class TicTacToe {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Tic Tac Toe Game");
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter the name of player1 :");
+		String p1 = s.nextLine();
+		System.out.println("enter the name of player2 :");
+		String p2 = s.nextLine();
+		s.close();
 		char[][] board = new char[3][3];
 		showBoard(board);
+		choosingXY(p1,p2);
 
 	}
 
@@ -19,6 +26,19 @@ public class TicTacToe {
 			}
 			System.out.println();
 		}
+	}
+	public static void choosingXY(String p1,String p2) {
+		char c = '-';
+		boolean player1 = true;
+		if(player1) {
+			System.out.println(p1+"'s turn (x)");
+			c='x';
+		}
+			else {
+				System.out.println(p2+"'s turn (o)");
+				c='o';
+			}
+		
 	}
 
 }
