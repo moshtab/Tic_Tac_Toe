bbc67a085c73b5bff04cd9159485bf3435f7524e
moshtab <mdmohsin41439@gmail.com>
2021-08-01 21:28:25 +0530

    showing board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,10 +1,24 @@
 package bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToe {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Tic Tac Toe Game");
+		char[][] board = new char[3][3];
+		showBoard(board);
 
+	}
+
+	public static void showBoard(char[][] board) {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				board[i][j] = '-';
+				System.out.print("\t" + board[i][j]);
+			}
+			System.out.println();
+		}
 	}
 
 }
